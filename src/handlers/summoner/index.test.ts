e5062317131fccf7a229907c15d96734eeb38597
sandibosna@outlook.com <sandibosna@outlook.com>
2018-09-08 21:33:24 +0200

    improving summoner tests

--- src/handlers/summoner/index.test.ts
+++ src/handlers/summoner/index.test.ts
@@ -1,32 +1,31 @@
+import { expect } from "chai";
+import dotenv = require("dotenv");
 import "mocha";
+
 import { JarvanScript } from "../..";
 import { Regions } from "../../models";
 import { SummonerMethods } from "./";
 
-import dotenv = require("dotenv");
 dotenv.config();
 
 const wrapper = new JarvanScript(process.env.RIOT_API_KEY as string, Regions.EUW, {});
 
 describe("SummonerMethods", () => {
-  it("should get summoner by account id", (done) => {
-    SummonerMethods.getSummonerByAccountId(wrapper, 219406964)
-    .then((data) => {
-      done();
-    });
+  it("should get summoner by account id", async () => {
+    const summ = await SummonerMethods.getSummonerByAccountId(wrapper, 219406964);
+    expect(summ.accountId).to.be.a("number");
+    expect(summ.accountId).to.equal(219406964);
   });
 
-  it("should get summoner by summoner id", (done) => {
-    SummonerMethods.getSummonerById(wrapper, 69658457)
-    .then((data) => {
-      done();
-    });
+  it("should get summoner by summoner id", async () => {
+    const summ = await SummonerMethods.getSummonerById(wrapper, 69658457);
+    expect(summ.id).to.be.a("number");
+    expect(summ.id).to.equal(69658457);
   });
 
-  it("should get summoner by summoner name", (done) => {
-    SummonerMethods.getSummonerByName(wrapper, "IAmTheWhite")
-    .then((data) => {
-      done();
-    });
+  it("should get summoner by summoner name", async () => {
+    const summ = await SummonerMethods.getSummonerByName(wrapper, "IAmTheWhite");
+    expect(summ.name).to.be.a("string");
+    expect(summ.id).to.equal(69658457);
   });
 });
